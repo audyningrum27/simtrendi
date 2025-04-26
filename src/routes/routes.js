da314d3510223bfe9a3ae328b0cdae9db71f429e
Ariefyusufwinata <winata.arief26@gmail.com>
2025-04-26 11:30:17 +0700

    refactor: for character

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,22 +1,22 @@
-import express from 'express';
-import authRoutes from '../services/auth.js';
-import pegawaiRoutes from '../services/pegawai.js';
-import gajiRoutes from '../services/gaji.js';
-import presensiRoutes from '../services/presensi.js';
-import cutiRoutes from '../services/cuti.js';
-import pelatihanRoutes from '../services/pelatihan.js';
-import roleDataRoutes from '../services/role.js';
-import notifikasiRoutes from '../services/notifikasi.js';
+import express from "express";
+import authRoutes from "../services/auth.js";
+import pegawaiRoutes from "../services/pegawai.js";
+import gajiRoutes from "../services/gaji.js";
+import presensiRoutes from "../services/presensi.js";
+import cutiRoutes from "../services/cuti.js";
+import pelatihanRoutes from "../services/pelatihan.js";
+import roleDataRoutes from "../services/role.js";
+import notifikasiRoutes from "../services/notifikasi.js";
 
 const router = express.Router();
 
-router.use('/auth', authRoutes);
-router.use('/data_pegawai', pegawaiRoutes);
-router.use('/data_gaji', gajiRoutes);
-router.use('/data_presensi', presensiRoutes);
-router.use('/data_cuti', cutiRoutes);
-router.use('/data_pelatihan', pelatihanRoutes);
-router.use('/data_role', roleDataRoutes);
-router.use('/data_notifikasi', notifikasiRoutes);
+router.use("/auth", authRoutes);
+router.use("/data_pegawai", pegawaiRoutes);
+router.use("/data_gaji", gajiRoutes);
+router.use("/data_presensi", presensiRoutes);
+router.use("/data_cuti", cutiRoutes);
+router.use("/data_pelatihan", pelatihanRoutes);
+router.use("/data_role", roleDataRoutes);
+router.use("/data_notifikasi", notifikasiRoutes);
 
 export default router;
