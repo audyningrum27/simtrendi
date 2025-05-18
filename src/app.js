a2f10b1149e5999041bacd365cb9c54b147051ef
Ariefyusufwinata <winata.arief26@gmail.com>
2025-05-18 16:04:45 +0700

    refactor: cors

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
 
-import routes from './routes/routes.js';
+import routes from "./routes/routes.js";
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://fe-simatren.riset-d3rpla.com",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json({ limit: "20mb" }));
 
-app.get('/', (req, res) => {
-    res.status(200).json({ 
-        code: 200,
-        message: 'Selamat Datang di API S!MATREN' 
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    code: 200,
+    message: "Selamat Datang di API S!MATREN",
+  });
 });
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 export default app;
