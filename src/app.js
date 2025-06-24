d25215844c7df2ff97c29674e3e136e524dff6e4
Ariefyusufwinata <winata.arief26@gmail.com>
2025-06-24 20:41:37 +0700

    refactor: cors origin update

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
+      "https://simatren.cloud",
       "https://fe-simatren.riset-d3rpla.com",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
